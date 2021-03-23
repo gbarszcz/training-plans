890cf280dcbba691b4284f7d272de64411f1fa43
Gabriela Barszcz <gabrielabarszcz2@gmail.com>
2021-03-23 17:18:06 +0100

    Added missing implementation for AccountService

--- src/main/java/com/tcGroup/trainingCenter/account/AccountService.java
+++ src/main/java/com/tcGroup/trainingCenter/account/AccountService.java
@@ -1,8 +1,9 @@
 package com.tcGroup.trainingCenter.account;
 
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 
 @Service("accountService")
-public interface AccountService {
+public interface AccountService extends UserDetailsService {
 
 }
--- src/main/java/com/tcGroup/trainingCenter/account/AccountServiceImpl.java
+++ src/main/java/com/tcGroup/trainingCenter/account/AccountServiceImpl.java
@@ -1,8 +1,34 @@
 package com.tcGroup.trainingCenter.account;
 
+import com.tcGroup.trainingCenter.role.Role;
 import com.tcGroup.trainingCenter.utility.logic.AbstractService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class AccountServiceImpl extends AbstractService implements AccountService {
 
+    @Autowired
+    private AccountRepository accountRepository;
 
+    @Override
+    @Transactional(readOnly = true)
+    public UserDetails loadUserByUsername(String accountEmail) throws UsernameNotFoundException {
+        Account account = accountRepository.findByAccountEmail(accountEmail);
+        if (account == null) {
+            throw new UsernameNotFoundException("Account with email address " + accountEmail + " not found.");
+        }
+
+        Set<Role> roles = account.getRoles();
+        Set<GrantedAuthority> grantedAuthorities = roles.stream()
+                .map(role -> new SimpleGrantedAuthority(role.getRoleName())).collect(Collectors.toSet());
+
+        return new org.springframework.security.core.userdetails.User(account.getAccountEmail(), account.getAccountPassword(), grantedAuthorities);
+    }
 }
