5bce114b4bd6b2ecf56f9519956e4bd61210ac2b
Gabriela Barszcz <gabrielabarszcz2@gmail.com>
2021-04-07 18:10:01 +0200

    [C-002B] Simple error handling and validations

--- src/main/java/com/tcGroup/trainingCenter/user/controller/RegistrationController.java
+++ src/main/java/com/tcGroup/trainingCenter/user/controller/RegistrationController.java
@@ -1,20 +1,15 @@
 package com.tcGroup.trainingCenter.user.controller;
 
-import java.util.regex.Pattern;
-
-import com.tcGroup.trainingCenter.user.entity.AccountData;
 import com.tcGroup.trainingCenter.user.request.RegistrationRequest;
 import com.tcGroup.trainingCenter.user.service.AccountManagementService;
 import com.tcGroup.trainingCenter.user.service.RegistrationService;
-import com.tcGroup.trainingCenter.utility.AppParams;
 import com.tcGroup.trainingCenter.utility.logic.AbstractController;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
+
+import javax.validation.Valid;
 
 @RestController
 public class RegistrationController extends AbstractController {
@@ -26,27 +21,7 @@ public class RegistrationController extends AbstractController {
     public RegistrationService registrationService;
 
     @PostMapping("/register")
-    public Long register(@RequestBody RegistrationRequest registrationRequest) {
-        Pattern patternEmail = Pattern.compile(AppParams.EMAIL_REGEX);
-        Pattern patternPassword = Pattern.compile(AppParams.PASSWORD_REGEX);
-
-        String email = registrationRequest.getEmail();
-        String password = registrationRequest.getPassword();
-
-        AccountData existingAccount = accountService.getAccountByEmail(email);
-
-        if (existingAccount != null) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Account with given email already exist");
-        }
-
-        if (email == null || email.isBlank() || !patternEmail.matcher(email).matches()) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Email invalid");
-        }
-
-        if (password == null || password.isBlank() || !patternPassword.matcher(password).matches()) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Password invalid");
-        }
-
+    public Long register(@RequestBody @Valid RegistrationRequest registrationRequest) {
         Long accountID = registrationService.register(registrationRequest);
 
         return accountID;
