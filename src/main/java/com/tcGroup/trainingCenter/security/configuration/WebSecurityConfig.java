8b5a43502931302b22f04292178030954cdd6e7c
kartoner <jakub.michal.ostrowski@gmail.com>
2021-04-13 16:05:32 +0200

    M-008B

--- src/main/java/com/tcGroup/trainingCenter/security/configuration/WebSecurityConfig.java
+++ src/main/java/com/tcGroup/trainingCenter/security/configuration/WebSecurityConfig.java
@@ -1,6 +1,14 @@
 package com.tcGroup.trainingCenter.security.configuration;
 
 import com.tcGroup.trainingCenter.security.filter.CORSFilter;
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.tcGroup.trainingCenter.user.provider.CustomAuthProvider;
+import com.tcGroup.trainingCenter.user.response.AuthResponse;
 import com.tcGroup.trainingCenter.user.service.AccountManagementService;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,8 +19,12 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.session.SessionManagementFilter;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 @Configuration
 @EnableWebSecurity
@@ -21,12 +33,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
      @Autowired
      private AccountManagementService accountService;
 
+     @Autowired
+     private CustomAuthProvider authProvider;
+
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-         auth.userDetailsService(accountService).passwordEncoder(bCryptPasswordEncoder());
+         auth.userDetailsService(accountService).passwordEncoder(bCryptPasswordEncoder())
+            .and().authenticationProvider(authProvider);
     }
 
-    @Bean
+    @Bean(value = "bCryptPasswordEncoder")
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder(10);
     }
@@ -43,9 +59,47 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.addFilterBefore(corsFilter(), SessionManagementFilter.class).authorizeRequests().antMatchers("/resources/**", "/scss/**", "/webjars/**", "/", "/index", "/register", "/exercises/**", "/exercise/**", "/tags").permitAll()
-                .anyRequest().authenticated().and().csrf().ignoringAntMatchers("/phpmyadmin/**", "/register", "/exercises/**", "/exercise/**", "/tags").and().headers()
-                .frameOptions().sameOrigin();
+        http.addFilterBefore(corsFilter(), SessionManagementFilter.class).httpBasic().and().authorizeRequests().antMatchers("/resources/**", "/scss/**", "/webjars/**", "/error", "/", "/index", "/register", "/login", "/exercises/**", "/exercise/**", "/tags").permitAll()
+                .and().csrf().ignoringAntMatchers("/phpmyadmin/**", "/register", "/login*", "/exercises/**", "/exercise/**", "/tags").and().headers()
+                .frameOptions().sameOrigin()
+                .and().formLogin()
+                    .loginPage("/login")
+                    .usernameParameter("email")
+                    .passwordParameter("password")
+                    .successHandler(successHandler())
+                    .failureHandler(failureHandler())
+                    .permitAll()
+                .and().logout()
+                    .logoutUrl("/logout")
+                    .logoutSuccessUrl("/")
+                    .invalidateHttpSession(true)
+                    .deleteCookies("JSESSIONID")
+                    .permitAll();
     }
 
+    private AuthenticationSuccessHandler successHandler() {
+        return new AuthenticationSuccessHandler() {
+            @Override
+            public void onAuthenticationSuccess(HttpServletRequest httpServletRequest,
+                    HttpServletResponse httpServletResponse, Authentication authentication)
+                    throws IOException, ServletException {
+                httpServletResponse.getWriter().append(new AuthResponse("OK").toString());
+                httpServletResponse.setContentType("application/json");
+                httpServletResponse.setStatus(200);
+            }
+        };
+    }
+    
+    private AuthenticationFailureHandler failureHandler() {
+        return new AuthenticationFailureHandler() {
+            @Override
+            public void onAuthenticationFailure(HttpServletRequest httpServletRequest,
+                    HttpServletResponse httpServletResponse, AuthenticationException e)
+                    throws IOException, ServletException {
+                httpServletResponse.getWriter().append(new AuthResponse(e.getMessage()).toString());
+                httpServletResponse.setContentType("application/json");
+                httpServletResponse.setStatus(401);
+            }
+        };
+    }
 }
---
+++ src/main/java/com/tcGroup/trainingCenter/user/response/AuthResponse.java
@@ -0,0 +1,28 @@
+package com.tcGroup.trainingCenter.user.response;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+@Getter
+@EqualsAndHashCode
+@AllArgsConstructor
+public class AuthResponse {
+    
+    private String message;
+
+    @Override
+    public String toString() {
+        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        try {
+            return ow.writeValueAsString(this);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
