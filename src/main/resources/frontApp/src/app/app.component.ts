9d9c7d9fb6d04887da2638c0f564d40ef4eaa696
JaroslawTusinski <jarrodtusinski@gmail.com>
2021-04-23 16:24:56 +0200

    Api connection

--- src/main/resources/frontApp/src/app/app.component.ts
+++ src/main/resources/frontApp/src/app/app.component.ts
@@ -4,7 +4,7 @@ import {Navigation} from './models/Navigation';
 import {Router} from '@angular/router';
 import {ISocialMedia} from './models/ISocialMedia';
 import {IPageContent} from './models/IPageContent';
-import { Location } from '@angular/common';
+import {Location} from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -13,19 +13,13 @@ import { Location } from '@angular/common';
 export class AppComponent implements OnInit {
   navigation: Navigation | null = null;
   socialMedia: ISocialMedia[] = [];
-  pageContent: IPageContent[] = [];
+  mockPageContent: IPageContent[] = [];
+  pageContent: Response | null = null;
   pageType = 'section';
   url: string;
 
   constructor(private appService: AppService, private router: Router, private location: Location) {
     this.url = location.path() || '/';
-  }
-
-  changeURL(URL: string): void {
-    this.url = URL;
-    this.router.navigate([this.url]);
-
-    this.prepareFields();
   }
 
   ngOnInit(): void {
@@ -36,15 +30,36 @@ export class AppComponent implements OnInit {
 
   prepareFields(): void {
     this.navigation = new Navigation(this.appService.getNavigation(this.url), this.url) || null;
+    const ENDPOINT = this.url.replace('/', '');
 
-    try {
-      const TMP = JSON.parse(this.appService.getPageContent(this.url));
-      this.pageType = TMP.type;
-      this.pageContent = TMP.sections;
-    }
-    catch (e) {
-      this.pageType = '';
-      this.pageContent = [];
-    }
+    // todo: finally this should be remove - just for mocking sites. ---
+    const PAGE_CONTENT_REQUEST = this.appService.getPageContent(this.url);
+    if (!!PAGE_CONTENT_REQUEST) {
+      try {
+        const TMP = JSON.parse(PAGE_CONTENT_REQUEST);
+        this.mockPageContent = TMP.sections;
+        this.pageType = ('sections' in this.mockPageContent) ? TMP.type : ENDPOINT;
+      } catch (e) {}
+      return;
+    } // ---
+
+    this.appService.apiGetRequest(ENDPOINT).subscribe(
+      (res: Response) => {
+        this.pageContent = res;
+        if (!('sections' in this.pageContent)) {
+          this.pageType = ENDPOINT;
+        }
+      },
+      (error: any) => {
+        console.error(error);
+      }
+    );
+  }
+
+  changeURL(URL: string): void {
+    this.url = URL;
+    this.router.navigate([this.url]);
+
+    this.prepareFields();
   }
 }
--- src/main/resources/frontApp/src/app/components/profile/profile.component.ts
+++ src/main/resources/frontApp/src/app/components/profile/profile.component.ts
@@ -1,15 +1,147 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnChanges} from '@angular/core';
+import {DatePipe} from '@angular/common';
+import {IAlert} from '../../models/IAlert';
+import {AppService} from '../../app.service';
 
 @Component({
   selector: 'user-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent implements OnInit {
+export class ProfileComponent implements OnChanges {
+  @Input() response: any | null = null;
+  userName = 'Undefined';
+  formData: any = {};
+  userData: any = {
+    user: [
+      {
+        value: '',
+        icon: 'bi-person',
+        placeholder: 'First name',
+        name: 'firstName',
+        type: 'text',
+        error: ''
+      },
+      {
+        value: '',
+        icon: 'bi-person',
+        placeholder: 'Last name',
+        name: 'lastName',
+        type: 'text',
+        error: ''
+      },
+      {
+        value: '',
+        icon: 'bi-person-badge',
+        require: true,
+        placeholder: 'Identifier',
+        name: 'identifier',
+        type: 'text',
+        error: ''
+      },
+    ],
+    about: [
+      {
+        value: '',
+        icon: 'bi-at',
+        require: true,
+        placeholder: 'Email',
+        name: 'email',
+        type: 'email',
+        error: ''
+      },
+      {
+        value: '',
+        icon: 'bi-calendar-event',
+        placeholder: 'Birthdate',
+        name: 'birthdate',
+        type: 'date',
+        error: ''
+      },
+      {
+        value: '',
+        text: 'About you',
+        placeholder: 'About you',
+        name: 'describe',
+        type: 'textarea',
+        error: ''
+      },
+    ]
+  };
+  alerts: IAlert[] = [];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private datePipe: DatePipe, private service: AppService) {
   }
 
+  ngOnChanges(): void {
+    this.response.createdAt = this.dateFormate(this.response?.createdAt);
+    this.response.updatedAt = this.dateFormate(this.response?.updatedAt);
+    this.userName = this.setUserName();
+  }
+
+  put(): void {
+    this.service.apiPutRequest('profile', this.formData)
+      .subscribe(
+        (res: any) => {
+          this.alerts.push({
+            id: this.alerts.length,
+            show: true,
+            header: 'Ok!',
+            text: 'Your changes have been saved',
+            level: 'success',
+            displayHideButton: true
+          });
+        },
+        (error: any) => {
+          if (error.status === 400) {
+            this.prepareErrorFields(error.error.errors);
+          } else {
+            this.alerts.push({
+              id: this.alerts.length,
+              show: true,
+              header: 'Sorry! We have encountered a problem...',
+              text: 'Please try again later :\'(',
+              level: 'danger',
+              displayHideButton: true
+            });
+          }
+        }
+      );
+  }
+
+  returnIfHasKay(obj: any, key: string): any | boolean {
+    return key in obj ? obj[key] : false;
+  }
+
+  private prepareErrorFields(errors: any[]): void {
+    ['user', 'about'].forEach((variant: string) => {
+      errors.forEach((error) => {
+        this.userData[variant.toString()].filter((input: any) => {
+          return input.name === error.field;
+        })[0].error = error.message;
+      });
+    });
+  }
+
+  private dateFormate(dateString: string | null): string {
+    if (!!dateString) {
+      return this.datePipe.transform(
+        new Date(dateString),
+        'MMMM d, y'
+      )?.toString() || 'Undefined';
+    }
+    return 'Undefined';
+  }
+
+  private setUserName(): string {
+    let firstName = 'firstName' in this.response ? this.response.firstName : '';
+    firstName = 'lastName' in this.response.lastName ? (
+      firstName !== '' ? `${firstName} ${this.response.lastName})` : this.response.lastName
+    ) : '';
+    if (!!firstName) {
+      return firstName;
+    }
+    firstName = this.response.identifier || 'Undefined';
+    return firstName;
+  }
 }
