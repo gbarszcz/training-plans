f58f160a76932d0b9f204f89b99d94675d410fd6
Gabriela Barszcz <gabrielabarszcz2@gmail.com>
2021-05-16 19:46:36 +0200

    C-010B - basic Swagger configuration

--- src/main/java/com/tcGroup/trainingCenter/security/configuration/WebSecurityConfig.java
+++ src/main/java/com/tcGroup/trainingCenter/security/configuration/WebSecurityConfig.java
@@ -53,11 +53,25 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected CORSLogoutSuccessHandler corsLogoutSuccessHandler() {
         return new CORSLogoutSuccessHandler();
     }
-
+    private static final String[] AUTH_WHITELIST = {
+            // -- Swagger UI v2
+            "/v2/api-docs",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/swagger-ui.html",
+            "/webjars/**",
+            // -- Swagger UI v3 (OpenAPI)
+            "/v3/api-docs/**",
+            "/swagger-ui/**"
+            // other public endpoints of your API may be appended to this array
+    };
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.addFilterBefore(corsFilter(), SessionManagementFilter.class).httpBasic().and().authorizeRequests()
             .antMatchers("/user", "/logout", "/profile").authenticated()
+            .antMatchers(AUTH_WHITELIST).anonymous()
             .antMatchers("/resources/**", "/scss/**", "/webjars/**", "/error", "/", "/index", "/register", "/login", "/exercises/**", "/exercise/**", "/tags", "/account/**").permitAll()
                 .and().csrf().ignoringAntMatchers("/phpmyadmin/**", "/register", "/login*", "/logout*", "/exercises/**", "/exercise/**", "/tags", "/account/**", "/profile")
                     .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
