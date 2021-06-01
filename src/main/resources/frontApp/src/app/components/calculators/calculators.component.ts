bb7b83aaafa3cf9dc3d0b92e3016ce897cfb7390
Krisu1007 <krystek10071@wp.pl>
2021-06-01 20:18:52 +0200

    add calculator views (BMI, CI) WIP

--- src/main/resources/frontApp/src/app/components/calculators/calculators.component.ts
+++ src/main/resources/frontApp/src/app/components/calculators/calculators.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {AppService} from "../../app.service";
+import {AppService} from '../../app.service';
 
 
 
@@ -10,19 +10,52 @@ import {AppService} from "../../app.service";
 })
 export class CalculatorsComponent implements OnInit {
   response: any | null = null;
-  weight = 0;
-  height = 0;
+  weightBMI = 0;
+  heightBMI = 0;
+  resultBMI = 0;
+  resultCI = 0;
+  weightCI = 0;
+  heightCI = 0;
+  hipCircumference = 0;
+  waist = 0;
 
   constructor(private service: AppService) {
-    //service.apiGetRequest()
-  }
-
-  public calculateBMI(weight: any, height: number): void {
-    this.weight = weight;
-    this.height = height;
   }
 
   ngOnInit(): void {
   }
 
+  public calculateBMI(weightBMI: number, heightBMI: number): void {
+    this.weightBMI = weightBMI;
+    this.heightBMI = heightBMI;
+    this.service.apiGetRequest('calculate', {params: {
+      calculatorType: 'BMI',
+      weight: this.weightBMI,
+      height: this.heightBMI}, observe: 'response'}).subscribe(
+        (res: any) => {
+        this.response = res;
+        this.resultBMI = this.response.body;
+      },
+      (error: any) => {
+        console.error(error);
+      }
+    );
+  }
+
+  public calculateCI(weightCI: number, heightCI: number): void {
+    this.weightCI = weightCI;
+    this.heightCI = heightCI;
+    this.service.apiGetRequest('calculate', {params: {
+        calculatorType: 'CI',
+        weight: this.weightCI,
+        height: this.heightCI}, observe: 'response'}).subscribe(
+      (res: any) => {
+        this.response = res;
+        this.resultCI = this.response.body;
+      },
+      (error: any) => {
+        console.error(error);
+      }
+    );
+  }
 }
